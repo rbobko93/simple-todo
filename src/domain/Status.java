55a3368f7568eea7abd132c323670d5473137b0a
Rafal Bobko <rbobko70@gmail.com>
2020-02-13 17:56:56 -0500

    Adding task and status

---
+++ src/domain/Status.java
@@ -0,0 +1,6 @@
+package domain;
+
+public enum Status {
+    IN_PROGRESS,
+    DONE;
+}
---
+++ src/domain/Task.java
@@ -0,0 +1,31 @@
+package domain;
+
+public class Task {
+    private String name;
+    private String description;
+    private Status status;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
