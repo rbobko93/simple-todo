19cb9aa694a686ecbc04da2daf32e579508dc609
Rafal Bobko <rbobko70@gmail.com>
2020-02-13 17:59:36 -0500

    Adding add task logic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import domain.Task;
+
 public class Main {
     public static void main(String[] args) {
+        TodoList todoList = new TodoList();
 
+        Task task = new Task("Nazwa1", "Desc");
+        todoList.addTask(task);
     }
 }
--- src/domain/Task.java
+++ src/domain/Task.java
@@ -5,6 +5,12 @@ public class Task {
     private String description;
     private Status status;
 
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.status = Status.IN_PROGRESS;
+    }
+
     public String getName() {
         return name;
     }
